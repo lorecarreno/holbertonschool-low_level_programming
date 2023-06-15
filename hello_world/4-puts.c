251fbd55f5f97cbfdb870fd075e32de844d28978
lorecarreno <rodelugaliza@gmail.com>
2023-06-15 16:07:37 -0700

    hello_world

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Description: This function print a message in console.
+ *
+ * Return: 0 on successful execution.
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
