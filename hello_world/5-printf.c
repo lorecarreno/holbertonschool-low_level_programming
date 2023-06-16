64ee916b3a7b9e4c8d66ef2c76def36c5f691c12
lorecarreno <rodelugaliza@gmail.com>
2023-06-15 21:26:09 -0700

    hello_world

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Description: This function print a message in console.
+ *
+ * Return: 0 on successful execution.
+ */
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+
+        return (0);
+}
